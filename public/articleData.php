3dd89861e3b60ab4048369a13aca9f4268df756d
Tim.tian <Tim.tian@mexgroup.com>
2017-05-24 12:51:10 +0800

    article add title

--- public/articleData.php
+++ public/articleData.php
@@ -1,14 +1,14 @@
 {
 	'blog':[
-		{'tittle':'HTML中图片自适应方案','time':'2017.05.24','href':'10','author':'tim'},
-		{'tittle':'模态框实现及背景透明，文字不透明效果','time':'2017.05.01','href':'9','author':'tim'},
-		{'tittle':'公告滚动效果的制作(js)','time':'2017.05.01','href':'8','author':'tim',},
-		{'tittle':'按钮覆盖效果','time':'2017.05.01','href':'7','author':'tim'},
-		{'tittle':'js生成验证码','time':'2017.05.01','href':'6','author':'tim'},
-		{'tittle':'HTML页面加载和解析流程','time':'2017.05.01','href':'5','author':'tim'},
-		{'tittle':'div+css清除浮动方法','time':'2017.05.01','href':'4','author':'tim'},
-		{'tittle':'CSS的继承','time':'2017.05.01','href':'3','author':'tim'},
-		{'tittle':'CSS导航菜单水平居中','time':'2017.05.01','href':'2','author':'tim'},		
-		{'tittle':'CSS3中常用单位的区别','time':'2017.05.01','href':'1','author':'tim'},
+		{'title':'HTML中图片自适应方案','time':'2017.05.24','href':'10','author':'tim'},
+		{'title':'模态框实现及背景透明，文字不透明效果','time':'2017.05.01','href':'9','author':'tim'},
+		{'title':'公告滚动效果的制作(js)','time':'2017.05.01','href':'8','author':'tim',},
+		{'title':'按钮覆盖效果','time':'2017.05.01','href':'7','author':'tim'},
+		{'title':'js生成验证码','time':'2017.05.01','href':'6','author':'tim'},
+		{'title':'HTML页面加载和解析流程','time':'2017.05.01','href':'5','author':'tim'},
+		{'title':'DIV+CSS清除浮动方法','time':'2017.05.01','href':'4','author':'tim'},
+		{'title':'CSS的继承','time':'2017.05.01','href':'3','author':'tim'},
+		{'title':'CSS导航菜单水平居中','time':'2017.05.01','href':'2','author':'tim'},		
+		{'title':'CSS3中常用单位的区别','time':'2017.05.01','href':'1','author':'tim'},
 	]
 }
