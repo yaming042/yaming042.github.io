14d2f49936365e4a9e850bac072031983e0a07a7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-29 12:15:24 +0000

    Deploy yaming042/yaming042.github.io to github.com/yaming042/yaming042.github.io.git:gh-pages

---
+++ js/tg.js
@@ -0,0 +1,66 @@
+(function(){
+    AV.init({
+        appId: window.APP_ID,
+        appKey: window.APP_KEY
+    });
+    // 显示次数
+    function showTime(Counter) {
+        var query = new AV.Query("Counter");
+
+        if( window.IS_POST ){
+            if($(".leancloud_visitors").length > 0){
+                var url = $(".leancloud_visitors").attr('id').trim();
+
+                query.equalTo("words", url).count().then(function (number) {
+                    $(document.getElementById(url)).text( number ? number : '--' );
+                }, function (error) {});
+            }
+        }else{
+            query.select(['words']).count().then(function(number){
+                $('.visitor_area').css('display', 'inline');
+                $('#visit_count').text( number || '--' );
+            }, function (error) {});
+        }
+    }
+    // 追加pv
+    function addCount(Counter) {
+        var url = $(".leancloud_visitors").length > 0 ? $(".leancloud_visitors").attr('id').trim() : window.location.href;
+        var Counter = AV.Object.extend("Counter");
+        var query = new Counter;
+        query.save({
+            words: url
+        }).then(function (object) {
+
+        })
+    }
+    function renderVisitor(Counter){
+        var query = new AV.Query("Counter"),
+            visitors = {};
+
+        query.select(['words']).each(function(v,i){
+            var words = v.attributes['words'];
+            
+            if( visitors.hasOwnProperty(words) ){
+                visitors[words]++;
+            }else{
+                visitors[words] = 1;
+            }
+        })
+        
+        var t = setTimeout(function(){
+            clearTimeout( t );
+            $('.visitor').each(function( index, item ){
+                var url = $(item).attr('id'),
+                    number = visitors[url];
+                
+                $(document.getElementById(url)).text( number ? number : '--' );
+            });
+        }, 1000);
+
+    }
+
+    var Counter = AV.Object.extend("Counter");
+    addCount(Counter);
+    showTime(Counter);
+    renderVisitor(Counter);
+})();
